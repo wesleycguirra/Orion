589976a3d97c06db8b4deb6d33e8ca5536be1405
Wesley Guirra <wesleycguirra@live.com>
2015-01-19 17:44:06 -0200

    Added orion files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title>Grupo Remax</title>
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/orion.css" />
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <!-- Custom styles for this template -->
+        <link rel="stylesheet" href="css/carousel.css" />
+    </head>
+    <body>
+       <section id="navbar">
+           <div class="container">
+               <div class="row">
+                   <div class="col-xs-8 col-md-2">
+                       <img class="logo" src="img/logo.png" alt="Grupo Remax">
+                   </div>
+                   <div class="col-xs-4 col-md-10">
+                   </div>
+               </div>
+           </div>
+       </section>
+       
+       
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
